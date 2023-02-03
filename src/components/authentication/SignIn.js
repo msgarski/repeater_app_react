866e7a8b318602905e6413eb627e072e3bcb5a22
msgarski <msgarski@gmail.com>
2023-02-03 17:39:13 +0100

    login error types handling

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -5,6 +5,8 @@ import { INITIAL_CREDENTIALS_STATE } from "../../general/constants";
 import { useContext, useState } from "react";
 import LoggedInContext from "../contexts/LoggedInContext";
 import InputField from "../forms/InputField";
+import { useReducer } from "react";
+import InfoModal from "../modals/InfoModal";
 
 const SignIn = () => {
   const [updateAccessToken, setUserId] = useContext(LoggedInContext);
@@ -12,9 +14,13 @@ const SignIn = () => {
   const [credentialsPack, setCredentialsPack] = useState(
     INITIAL_CREDENTIALS_STATE
   );
+  const [errorType, setErrorCode] = useState();
+  const clearErrors = () => {
+    setErrorCode(null);
+  };
 
   // todo below function could be reuseable..., but how?
-  // todo the same method exists in ForgotPassword component
+  // todo because the same method exists in ForgotPassword component
 
   const handleChangeInputField = (event) => {
     setCredentialsPack({
@@ -34,8 +40,9 @@ const SignIn = () => {
 
       updateAccessToken(response.data.token);
       setUserId(response.data.userId);
-    } catch (err) {
-      console.log("err", err);
+    } catch (error) {
+      console.log("err", error);
+      setErrorCode(error.response.status);
     }
   };
 
@@ -46,29 +53,37 @@ const SignIn = () => {
     // todo po udanym zalogowaniu, przekierować usera do PorchSite
   };
 
-  //           this.$store.dispatch("setTodayDate");
-
-  //         .catch((error) => {
-  //           switch (error.response.status) {
-  //             case 403:
-  //               this.errorCode = 403; // not getting here
-  //               break;
-  //             case 404:
-  //               this.errorCode = 404; // or here
-  //               break;
-  //             case 401:
-  //               this.errorCode = 401; // or here
-  //               break;
-  //             default:
-  //               console.log("some other error"); // end up here all the time
-  //               break;
-  //           }
-  //         });
-  //     },
-
+  //**************************************************************************************** */
+  // JSX code
+  //**************************************************************************************** */
   return (
     <>
       <h1 id="title">Logowanie</h1>
+      <div>
+        {
+          {
+            401: (
+              <InfoModal
+                message="Jeśli nie pamiętasz hasła, skorzystaj z opcji jego odzyskiwania"
+                action={clearErrors}
+              />
+            ),
+            403: (
+              <InfoModal
+                message="Konto nie zostało aktywowane, sprawdź swoją skrzynkę pocztową"
+                action={clearErrors}
+              />
+            ),
+            404: (
+              <InfoModal
+                message="Użytkownik o podanym adresie email, nie istnieje"
+                action={clearErrors}
+              />
+            ),
+          }[errorType]
+        }
+      </div>
+
       <div>
         <form onSubmit={handleSubmitEvent}>
           <InputField
@@ -90,20 +105,10 @@ const SignIn = () => {
           />
           <button type="submit">Zatwierdź</button>
         </form>
-
-        {/* if "errorCode == 401 lub 404" */}
-        <div>
-          <p>Nieprawidłowy Login lub hasło</p>
-        </div>
-        {/* if "errorCode == 403" */}
-        <div>
-          <p>Konto nie zostało aktywowane, sprawdź swoją skrzynkę pocztową</p>
-        </div>
       </div>
       <div>
         <div>
           <Link to="/">
-            {/* buttony tez reusable */}
             <button>Cofnij</button>
           </Link>
         </div>
