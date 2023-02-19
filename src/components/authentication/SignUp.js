c74ccd8f7e8074fa5c703ebdf9a537d22b8086d1
msgarski <msgarski@gmail.com>
2023-02-19 09:35:25 +0100

    before implementing reusable sections in signUp

--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -1,26 +1,23 @@
 import { useState, useEffect, useRef } from "react";
-import {
-  SPECIAL_CHARS_ALLOWED_IN_PASSWORD,
-  INITIAL_USER_SIGNUP_STATE,
-} from "../../general/constants";
 import { Link, useNavigate } from "react-router-dom";
 import InputField from "../forms/InputField";
 import InfoModal from "../modals/InfoModal";
+import ValidationUi from "../forms/ValidationUi";
+import { INITIAL_USER_SIGNUP_STATE } from "../../general/constants";
 import { checkStringForSpecialChar } from "../../general/validators";
 import {
   userExistsMessage,
   activationLinkMessage,
 } from "../../general/messages";
-import { type } from "@testing-library/user-event/dist/type";
 
 //************************************************************************ */
 // Function component
 //************************************************************************ */
 const SignUp = () => {
   const [signUpPack, setSignUpPack] = useState(INITIAL_USER_SIGNUP_STATE);
+  const [typeOfInputField, setTypeOfInputField] = useState("password");
   const navigate = useNavigate();
   const passRef = useRef();
-  const [typeOfInputField, setTypeOfInputField] = useState("password");
 
   useEffect(() => {
     let result = checkStringForSpecialChar(signUpPack.password);
@@ -28,16 +25,12 @@ const SignUp = () => {
   }, [signUpPack.password, typeOfInputField]);
 
   const handleInputFieldToHookObject = (event) => {
-    //
     setSignUpPack({
       ...signUpPack,
       [event.target.id]: event.target.value,
     });
   };
 
-  // const showHidePassword = (passRef) => {
-  //   console.log("co ma ref?: ", passRef.current);
-  // };
   //*************************************************************************** */
   // Http request method
   //*************************************************************************** */
@@ -46,11 +39,14 @@ const SignUp = () => {
   // Form submition method
   //*************************************************************************** */
   const handleSubmitSignUpForm = (event) => {
+    console.log("tworzenie konta...");
     event.preventDefault();
   };
+  //************************************************************************** */
+  // Validation rules
+  //************************************************************************** */
   const checkPass = () => {
-    //
-    console.log("sprawdzenie hasla: ", passRef.current.type);
+    console.log("uwidocznienie hasla: ", passRef.current.type);
     setTypeOfInputField(typeOfInputField ? "" : "password");
     console.log("typ: ", typeOfInputField);
   };
@@ -89,47 +85,33 @@ const SignUp = () => {
             value={signUpPack.password}
             onChange={handleInputFieldToHookObject}
           />
-          <button onClick={checkPass}>sprawdz!</button>
           <InputField
             title="Potwierdź hasło"
-            type="password"
+            type={typeOfInputField}
             name="password_confirmation"
             id="password_confirmation"
             value={signUpPack.password_confirmation}
             onChange={handleInputFieldToHookObject}
           />
-          <section>
-            <p>długość hasła - od 5 do 15 znaków</p>
-            <p>
-              obecność minimum jednego znaku specjalnego:{" "}
-              {SPECIAL_CHARS_ALLOWED_IN_PASSWORD}
-            </p>
-            <p>obecność minimum jednej cyfry</p>
-            <p>zgodność obu haseł</p>
-          </section>
+          <button onClick={checkPass}>zobacz hasło</button>
 
           <button> Stwórz konto</button>
         </form>
         {/* if="userId=='exists'" */}
-        <div>
-          <div>
-            <InfoModal
-              message={userExistsMessage}
-              action={() => {
-                navigate("/forgot");
-              }}
-            >
-              Odzyskiwanie hasła
-            </InfoModal>
-          </div>
-          <p>Popraw adres poczty elektronicznej</p>
-        </div>
+        <InfoModal
+          message={userExistsMessage}
+          action={() => {
+            navigate("/forgot");
+          }}
+        >
+          Odzyskiwanie hasła
+        </InfoModal>
 
-        <div>
-          <InfoModal message={activationLinkMessage}>
-            <a href="http://google.com">Wyjście</a>
-          </InfoModal>
-        </div>
+        <ValidationUi />
+
+        <InfoModal message={activationLinkMessage}>
+          <a href="http://google.com">Wyjście</a>
+        </InfoModal>
       </div>
 
       <div>
