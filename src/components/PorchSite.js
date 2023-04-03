b9c819a85aa3c3bebfc6dfcd41f99d13d036685c
msgarski <msgarski@gmail.com>
2023-04-03 11:46:48 +0200

    fast repeats list rendering finished

--- src/components/PorchSite.js
+++ src/components/PorchSite.js
@@ -5,18 +5,22 @@ import { useEffect, useState, useRef } from "react";
 import { shouldWeUpdateContextJWT } from "../general/axiosMethods";
 import { API_URL } from "../general/constants";
 
-import { addListCoursesWithRepeats } from "../store/slices/fastRepeatsSlice";
-import { useDispatch } from "react-redux";
+import { addListCoursesWithRepeatsNums } from "../store/slices/fastRepeatsSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 //**************************************************************************** */
 //  Main Block
 //**************************************************************************** */
 const PorchSite = () => {
   const dispatch = useDispatch();
+  const repeatsList = useSelector((state) => {
+    return state.repeats;
+  });
 
   const { token, userId, setTokenContext } = useAuthentication();
   const [courseList, setCourseList] = useState([]);
   const firstTimeRef = useRef(true);
+  const [showRepeats, setShowRepeats] = useState(false);
 
   // const [errorCode, setErrorCode] = useState(null);
 
@@ -39,8 +43,8 @@ const PorchSite = () => {
 
       setCourseList(response.data.payload);
 
+      // constant part of every http request:
       let result = shouldWeUpdateContextJWT(response, token);
-
       if (result) {
         setTokenContext(response.data.newToken);
       }
@@ -54,12 +58,12 @@ const PorchSite = () => {
   };
 
   useEffect(() => {
+    console.clear();
     if (firstTimeRef.current) {
       firstTimeRef.current = false;
-      console.log("znowu");
     } else {
-      console.log("courseList", courseList);
-      dispatch(addListCoursesWithRepeats(courseList));
+      dispatch(addListCoursesWithRepeatsNums(courseList));
+      console.log("state repeats", repeatsList);
     }
   }, [courseList, dispatch]);
 
@@ -72,19 +76,42 @@ const PorchSite = () => {
   //*************************************************************************** */
   return (
     <>
-      <div>
-        {/* router-link :to="'/repeating/' + courseId" */}
-
-        <button disabled>Powtórki na dowolną chwilę...</button>
-      </div>
-      <div>
-        <button disabled>Coś nie może już czekać...</button>
-      </div>
-      <div>
-        <Link to="/mainscreen">
-          <button>Przejdź do programu</button>
-        </Link>
-      </div>
+      {!showRepeats ? (
+        <div>
+          <div>
+            {/* router-link :to="'/repeating/' + courseId" */}
+            <button
+              disabled={!repeatsList}
+              onClick={() => setShowRepeats(true)}
+            >
+              Powtórki na dowolną chwilę...
+            </button>
+          </div>
+          <div>
+            <button disabled={true}>Coś nie może już czekać...</button>
+          </div>
+          <div>
+            <Link to="/mainscreen">
+              <button>Przejdź do programu</button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>Lista kursów z powtórkami</p>
+          <ul>
+            {repeatsList.map((el) => {
+              return (
+                <li key={el.name}>
+                  <div>{el.name}</div>
+                  <div>{el.repeats} powtórek</div>
+                </li>
+              );
+            })}
+          </ul>
+          <button onClick={() => setShowRepeats(false)}>Powrót</button>
+        </div>
+      )}
     </>
   );
 };
--- src/store/slices/fastRepeatsSlice.js
+++ src/store/slices/fastRepeatsSlice.js
@@ -4,12 +4,12 @@ const fastRepeatsSlice = createSlice({
   name: "repeats",
   initialState: [],
   reducers: {
-    addListCoursesWithRepeats: (state, action) => {
+    addListCoursesWithRepeatsNums: (state, action) => {
       state.length = 0;
       action.payload.forEach((el, i, a) => state.push(el));
     },
   },
 });
 
-export const { addListCoursesWithRepeats } = fastRepeatsSlice.actions;
+export const { addListCoursesWithRepeatsNums } = fastRepeatsSlice.actions;
 export const repeatsReducer = fastRepeatsSlice.reducer;
